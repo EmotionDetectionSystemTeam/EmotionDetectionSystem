4cb0175e8f42b1420a1e1f2649123fe88ee98c5e
AlvoBen <alvo@post.bgu.ac.il>
2024-06-19 16:56:21 +0300

    fix

--- EmotionDetectionSystem/DomainLayer/Repos/EnrollmentSummaryRepo.cs
+++ EmotionDetectionSystem/DomainLayer/Repos/EnrollmentSummaryRepo.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using EmotionDetectionSystem.DomainLayer.objects;
-
 namespace EmotionDetectionSystem.DomainLayer.Repos;
-
 public class EnrollmentSummaryRepo : IRepo<EnrollmentSummary>
 {
     private string _lessonId;
     private List<EnrollmentSummary> _enrollmentSummaries;
     
-
     public EnrollmentSummaryRepo(string lessonId)
     {
         _lessonId = lessonId;
@@ -19,7 +16,6 @@ public class EnrollmentSummaryRepo : IRepo<EnrollmentSummary>
     {
         return _enrollmentSummaries;
     }
-
     public EnrollmentSummary GetById(string id)
     {
         throw new NotImplementedException();
@@ -27,11 +23,10 @@ public class EnrollmentSummaryRepo : IRepo<EnrollmentSummary>
     public Dictionary<string, List<string>> GetStudentWiningEmotions()
     {
         Dictionary<string, List<string>> studentWiningEmotions = new Dictionary<string, List<string>>();
-
         foreach (var enrollmentSummary in _enrollmentSummaries)
         {
             string studentEmail = enrollmentSummary.Student.Email;
-            List<string> studentEmotions = enrollmentSummary.GetAllWiningEmotionData();
+            List<string> studentEmotions = enrollmentSummary.getAllWiningEmotionData();
 
             if (!studentWiningEmotions.ContainsKey(studentEmail))
             {
@@ -42,51 +37,40 @@ public class EnrollmentSummaryRepo : IRepo<EnrollmentSummary>
                 studentWiningEmotions[studentEmail].AddRange(studentEmotions);
             }
         }
-
         return studentWiningEmotions;
     }
-
-
     public void Add(EnrollmentSummary item)
     {
         _enrollmentSummaries.Add(item);
     }
-
     public void Update(EnrollmentSummary item)
     {
         throw new NotImplementedException();
     }
-
     public void Delete(string id)
     {
         throw new NotImplementedException();
     }
-
     public bool ContainsID(string id)
     {
         throw new NotImplementedException();
     }
-
     public bool ContainsValue(EnrollmentSummary item)
     {
         throw new NotImplementedException();
     }
-
     public void ResetDomainData()
     {
         throw new NotImplementedException();
     }
-
     public void Clear()
     {
         throw new NotImplementedException();
     }
-
     public bool ContainStudent(Student student)
     {
         return _enrollmentSummaries.Exists(x => x.Student.Email.Equals(student.Email));
     }
-
     public void PutEmotionData(string userEmail, EmotionData emotionData)
     {
         var enrollmentSummary = _enrollmentSummaries.Find(x => x.Student.Email.Equals(userEmail));
@@ -96,17 +80,15 @@ public class EnrollmentSummaryRepo : IRepo<EnrollmentSummary>
         }
         enrollmentSummary.AddEmotionData(emotionData);
     }
-
     public IEnumerable<EmotionData> GetEmotionDataEntries()
     {
         var emotionsData = new List<EmotionData>();
         return emotionsData.Concat(_enrollmentSummaries.SelectMany(x => x.GetAllEmotionData()));
     }
-
     public IEnumerable<EnrollmentSummary> GetEnrollmentSummariesWithData()
     {
         return _enrollmentSummaries
-            .Where(enrollmentSummary => enrollmentSummary.GetFirstNotSeenEmotionData() != null).ToList();
+            .Where(enrollmentSummary => enrollmentSummary.PeekFirstNotSeenEmotionData() != null).ToList();
     }
 
     public Dictionary<Student,EnrollmentSummary> GetStudentsEmotions()

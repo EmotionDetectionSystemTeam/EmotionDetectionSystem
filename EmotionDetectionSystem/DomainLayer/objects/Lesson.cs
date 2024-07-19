47c572ba7780129144cfd43c4916158752f6dee9
tamuz <tamuzgindes98@gmail.com>
2024-07-19 16:04:17 +0300

    Add solution and fixes

--- EmotionDetectionSystem/DomainLayer/objects/Lesson.cs
+++ EmotionDetectionSystem/DomainLayer/objects/Lesson.cs
@@ -181,7 +181,7 @@ namespace EmotionDetectionSystem.DomainLayer.objects
         /// <returns><c>true</c> if the viewer is allowed; otherwise, <c>false</c>.</returns>
         public bool IsAllowedToViewStudentsData(Viewer viewer)
         {
-            return _viewers.Contains(viewer) || viewer == _teacher;
+            return Viewers.Contains(viewer) || viewer == _teacher;
         }
 
         /// <summary>
